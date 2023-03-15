4fa7dbaf2b756933c21d8a9ce3658ba3878f3795
DENIS <den465341@gmail.com>
2023-03-15 23:38:57 +0300

    feat: implement categories functionality

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,14 +1,31 @@
+import React, { useState } from 'react';
+
 function Categories() {
+  const [activeCategoryId, setActiveCategoryId] = useState(0);
+
+  const categoryData = [
+    { id: 0, name: 'Все' },
+    { id: 1, name: 'Мясные' },
+    { id: 2, name: 'Вегетарианская' },
+    { id: 3, name: 'Гриль' },
+    { id: 4, name: 'Острые' },
+    { id: 5, name: 'Закрытые' },
+  ];
+
+  const categoryList = categoryData.map((c) => {
+    return (
+      <li
+        className={c.id === activeCategoryId ? 'active' : ''}
+        key={c.id}
+        onClick={() => setActiveCategoryId(c.id)}>
+        {c.name}
+      </li>
+    );
+  });
+
   return (
-    <div class="categories">
-      <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
-      </ul>
+    <div className="categories">
+      <ul>{categoryList}</ul>
     </div>
   );
 }
