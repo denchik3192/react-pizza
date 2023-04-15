a223a28c1e6480268e09346ac17f45b8ef609d8d
DENIS <den465341@gmail.com>
2023-04-15 18:27:44 +0300

    fix: fix skeleton

--- src/components/PizzaBlock/Skeleton.tsx
+++ src/components/PizzaBlock/Skeleton.tsx
@@ -6,7 +6,7 @@ const Skeleton = (props: any) => (
     className="pizza-block"
     speed={2}
     width={280}
-    height={500}
+    height={465}
     viewBox="0 0 280 500"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -81,6 +81,9 @@ function Home() {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{status === 'loading' ? skeleton : pizzas}</div>
+      {/* <Skeleton> </Skeleton>
+      <Skeleton> </Skeleton> */}
+
       <Pagination />
     </>
   );
